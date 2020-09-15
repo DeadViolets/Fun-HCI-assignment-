d9e03eaa2ab4ea7a9863c9f242f11da35598f7f3
Asger Phillip Andersen <asgerpandersen@gmail.com>
2020-09-15 14:23:44 +0200

    Her HH

--- code - Copy.js
+++ code - Copy.js
@@ -1,28 +1,36 @@
 let experimentActive = false;
-let speed = 3;
+let speed = 5;
 let direction = 1;
 let hit = 0;
 let miss = 0;
 let movingBox = document.getElementById("movingDiv");
 let target = document.getElementById("targetDiv");
-let changeStartingSide = 0;
+let changeStartingSide = true;
+let changeStartingSideCount = 0;
+let invertColors = false;
+let invertColorsCount = 0;
+let invertColorsCheck = 1;
 let startingSide = 1;
 
 const screenWidth = window.innerWidth;
-const rightDiv = document.getElementById("rightDiv");
 const leftDiv = document.getElementById("leftDiv");
 const p_hit = document.getElementById("hit");
 const p_miss = document.getElementById("miss");
 const p_time = document.getElementById("time");
+const p_mean = document.getElementById("mean");
 const p_speed = document.getElementById("pSpeed");
+const p_invert = document.getElementById("pInvert");
 const b_decSpeed = document.getElementById("decreaseSpeed");
 const b_incSpeed = document.getElementById("increaseSpeed");
+const b_falseInvert = document.getElementById("falseInvert");
+const b_trueInvert = document.getElementById("trueInvert");
 const startingLeft = leftDiv.offsetLeft;
 const startingRight = startingLeft + leftDiv.offsetWidth - movingBox.offsetWidth;
 
 async function startExperiment() {
     experimentActive = true;
-    changeStartingSide = Math.floor(Math.random() * 5 + 2);
+    changeStartingSideCount = newStartingSide();
+    invertColors = newInvert();
 
     if(movingBox) {
         let xPos = 0;
@@ -40,7 +48,8 @@ async function startExperiment() {
 }
 
 function checkTarget() {
-    if (movingBox.offsetLeft >= target.offsetLeft && movingBox.offsetLeft + movingBox.offsetWidth <= target.offsetLeft + target.offsetWidth) {
+    if (movingBox.offsetLeft >= target.offsetLeft 
+        && movingBox.offsetLeft + movingBox.offsetWidth <= target.offsetLeft + target.offsetWidth) {
         hit++;
         p_hit.innerHTML = "Hit: " + hit;
     } else {
@@ -48,16 +57,36 @@ function checkTarget() {
         p_miss.innerHTML = "Miss: " + miss;
     }
 
-    changeStartingSide--;
-    p_time.innerHTML = "changeStartingSide: " + changeStartingSide;
-    if(changeStartingSide == 0) {
-        startingSide = -startingSide;
-        changeStartingSide = Math.floor(Math.random() * 2 + 5);
+    if(changeStartingSide == true) {
+        changeStartingSideCount--;
+        p_time.innerHTML = "changeStartingSideCount: " + changeStartingSideCount;
+        if(changeStartingSideCount == 0) {
+            startingSide = -startingSide;
+            changeStartingSideCount = newStartingSide();
+        }
+        if(startingSide == 1) {
+            movingBox.style.left = startingLeft;
+        } else {
+            movingBox.style.left = startingRight;
+        }
     }
-    if(startingSide == 1) {
-        movingBox.style.left = startingLeft;
-    } else {
-        movingBox.style.left = startingRight;
+
+    if(invertColors == true) {
+        invertColorsCount--;
+        p_mean.innerHTML = "Invert count: " + invertColorsCount;
+        if(invertColorsCount == 0) {
+            invertColorsCheck = -invertColorsCheck;
+            invertColorsCount = newInvert();
+        }
+        if(invertColorsCheck == 1) {
+            movingBox.style.background = "blue";
+            leftDiv.style.background = "black";
+            target.style.background = "blue";
+        } else {
+            movingBox.style.background = "black";
+            leftDiv.style.background = "blue";
+            target.style.background = "black";
+        }
     }
 }
 
@@ -96,6 +125,18 @@ function onKey(e) {
     }
 }
 
+function getRandomNumber(lower, upper) {
+    return Math.floor(Math.random() * (upper - lower) + lower);
+}
+
+function newStartingSide() {
+    return getRandomNumber(2, 7);
+}
+
+function newInvert() {
+    return getRandomNumber(7, 10);
+}
+
 b_decSpeed.onclick = function fIncSpeed() {
     if(speed == 0) {
         return;
@@ -107,4 +148,14 @@ b_decSpeed.onclick = function fIncSpeed() {
 b_incSpeed.onclick = function fDecSpeed() {
     speed++;
     p_speed.innerHTML = "Speed: " + speed;
+}
+
+b_falseInvert.onclick = function fFalseInvert() {
+    invertColors = false;
+    p_invert.innerHTML = "Invert : nej";
+}
+
+b_trueInvert.onclick = function fFalseInvert() {
+    invertColors = true;
+    p_invert.innerHTML = "Invert: ja";
 }
